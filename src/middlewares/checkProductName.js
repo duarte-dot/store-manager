bfe686fdd06c73bb7c4388a0f1fff89a9bde9a0f
duarte <gabrieldvr@outlook.com>
2023-04-26 19:01:04 -0300

    requisito 4

---
+++ src/middlewares/checkProductName.js
@@ -0,0 +1,12 @@
+const checkProductName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
+  if (name.trim().length <= 5) {
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
+  }
+  next();
+};
+
+module.exports = checkProductName;
--- src/routes/productsRoute.js
+++ src/routes/productsRoute.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const productsControllers = require('../controllers/productsControllers');
 
 const checkProductID = require('../middlewares/checkProductID');
+const checkProductName = require('../middlewares/checkProductName');
 
 router.get('/', productsControllers.getAllProducts);
 router.get('/:id', checkProductID, productsControllers.getProductByID);
-router.post('/', productsControllers.createNewProduct);
+router.post('/', checkProductName, productsControllers.createNewProduct);
 
 module.exports = router;
