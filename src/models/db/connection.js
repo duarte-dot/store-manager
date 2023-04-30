9bc0542d27520543f4ec793591214f767b2aaf7d
duarte <gabrieldvr@outlook.com>
2023-04-30 13:54:40 -0300

    .env (FIX)

--- src/models/db/connection.js
+++ src/models/db/connection.js
@@ -4,8 +4,7 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  port: 3306,
-  database: 'StoreManager',
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = connection;
